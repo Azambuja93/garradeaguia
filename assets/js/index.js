6e927cc2c4568c57a07999434b21b273477d7426
Azambuja93 <rafael.estevam.93@gmail.com>
2024-03-27 22:51:05 -0300

    atualizações site mapa e layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,11 +26,11 @@ let map;
 
 async function initMap() {
     const coords = [
-        [{lat: -21.7846475, lng: -48.1797056}, "Local 1"],
-        [{lat: -21.7986507, lng: -48.1783277}, "Local 2"],
-        [{lat: -21.7887224, lng: -48.1909447}, "Local 3"],
+        [{ lat: -21.781266359055095, lng: -48.17669568546269 }, "Araraquara-SP Garra de Águia"],
+        [{ lat: -21.784388211355598, lng: -48.18026546766289 }, "Meu Endereço Aza teste 2"]
     ];
-
+    
+    
     // Request needed libraries.
     const { Map } = await google.maps.importLibrary("maps");
     const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
@@ -38,7 +38,7 @@ async function initMap() {
 
     map = new Map(document.getElementById("map"), {
         zoom: 15,
-        center: {lat: -21.7846475, lng: -48.1797056},
+        center: {lat: -21.781266359055095, lng: -48.17669568546269},
         mapId: "Mapa",
     });
 
@@ -58,13 +58,25 @@ async function initMap() {
             infoWindow.setContent(marker.getTitle());
             infoWindow.open(marker.getMap(), marker);
         });
-    });
 
-    const marker = new AdvancedMarkerView({
-        map: map,
-        position: position,
-        title: "Mapa",
     });
 }
 
 initMap();
+
+// foco ao escolher endereço no mapa
+
+function moveMapAndScroll(lat, lng) {
+    // Move o mapa para as coordenadas específicas
+    map.panTo({lat: lat, lng: lng});
+    
+    // Aguarde o mapa mover para então deslocar a visualização da página até o mapa
+    // Isso é útil se o movimento do mapa for animado e levar algum tempo
+    setTimeout(() => {
+        const mapElement = document.getElementById('encontrar-academia'); // Substitua 'mapaLocal' pelo ID real do seu elemento de mapa
+        if (mapElement) {
+            mapElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, 300); // Ajuste o tempo de espera conforme necessário
+}
+
